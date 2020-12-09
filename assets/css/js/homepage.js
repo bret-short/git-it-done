0dcfa18543bfdd9b5a66cb0ea0b8b02a0aa99df1
Bret Short <bretshort@Ellens-MacBook-Pro.local>
2020-12-09 16:57:52 -0700

    getting error

--- assets/css/js/homepage.js
+++ assets/css/js/homepage.js
@@ -4,10 +4,31 @@ var getUserRepos = function (user) {
 
     //make a request to the url
     fetch(apiUrl).then(function(response) {
+        console.log(response);
         response.json().then(function(data) {
             console.log(data);
         });
     });
 };
 
-getUserRepos("bret-short");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } 
+    else {
+        alert("Please enter a GitHub username");
+    }
+
+    console.log(event);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
