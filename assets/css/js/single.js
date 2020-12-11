c798b154406c7db149a670650bc1622ff79673f4
Bret Short <bretshort@Ellens-MacBook-Pro.local>
2020-12-11 14:21:56 -0700

    adding issue fix

--- assets/css/js/single.js
+++ assets/css/js/single.js
@@ -2,6 +2,18 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoName = function() {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if(repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  }
+  else {
+    document.location.replace("./index.html");
+  }
+};
+
 var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -81,4 +93,6 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoIssues();
+
+getRepoName();
